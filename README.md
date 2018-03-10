78fa13aa21e82213a08dc2eaa3fedb87d4c02798
BabesGotByte <37226108+BabesGotByte@users.noreply.github.com>
2018-03-10 10:29:45 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Blinds-Navigator
+# Introduction-
+This device contains an arduino, an ultrasonic sensor, an switch and a buzzer. It simply aims to help blind people. A person who can't see ,can feel safer with the help of our gadget. The principle of this gadget is simple.The closer the person is from an object,the louder will the buzzer of the gadget ring. In this way, it helps them avoid collision with anything and hence avoiding accidents. This way our gadget paves way for the blind people of our society to become more independent as one of the main factors that make a blind person dependent is their inability to see their surroundings which makes them so much accident prone. Our gadget is one of the technologies for this new era that will help people feel more equal and confident by helping them overcome their liabilities.Because we believe that every person has the right to dream freely and also feel confident about achieving them.
+
+
+
+
+![imshe](./images/picc1)
+
+
+# How this device works-
+In sensors we are having a triggered and an echo pin. We will connect those pins to the arduino at a particular number {provided at the start of the coding). By this we can calculate the distance between an object and a person by sending out a sound wave at a specific frequency and listening for that sound wave to bounce back.Since it is known that the speed of sound is 320m/s ,so we can take the time for the sound to return and multipy it by 0.032cm/(microsec) for the total trip. Total distance travel means that the sund wave traveled 2 times the distance to the object. So to find the distance to the object, simply divide the total distance by by 2. if distance is less than 1m ,one small beep and 4 big beeps ring(small beep means sound that rings for lesser time ). As distance keeps increasing by each meter, number of small beeps increase and big beeps decrease.
+
+ ![imshe](./images/picc2)
---
+++ ultrasonic_device.ino
@@ -0,0 +1,157 @@
+// trig is the source from which sound emmits out
+// echo is the destination, where the signals are 
+#define trig 9
+#define echo 10
+
+
+// prototype of a function
+int abe(void);
+
+//variables decelaration
+long duration;
+long distance;
+long y;
+boolean on=false;
+int buzzer=12;
+
+//a button to turn on and off the device
+int button=5;
+int buttonState=0;
+void setup() {
+  // put your setup code here, to run once:
+  // either the variable works as output or as input
+  pinMode(buzzer,OUTPUT);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  pinMode(button,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  long j,k;
+  //read button state
+  buttonState=digitalRead(button);
+  if(buttonState==HIGH){
+
+  //k is the distance of the object from the person
+      k=abe();
+  
+  //if the distance is less than 1 metre ,then first weep will be smaller and then continously for the remaining cases we get longer weeps
+   if(k<100){
+      digitalWrite(buzzer,HIGH);
+      delay(200);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(1000);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(1000);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(1000);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(1000);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+    }
+
+    //if the distance is less than 2 metre ,then first two weeps will be smaller and then continously for the remaining cases we get longer weeps
+    else if((k>=100)&&(k<200)){
+      digitalWrite(buzzer,HIGH);
+      delay(200);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(200);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(1000);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(1000);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(1000);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+    }
+
+    
+    //if the distance is less than 3 metre ,then first three weeps will be smaller and then continously for the remaining cases we get longer weeps
+    else if((k>=200)&&(k<300)){
+      digitalWrite(buzzer,HIGH);
+      delay(200);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(200);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(200);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(1000);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+       digitalWrite(buzzer,HIGH);
+      delay(1000);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+    }
+
+    
+    //if the distance is above 3 metre or equal to 3 metre ,then first four weeps will be smaller and then continously for the remaining cases we get longer weeps
+    else if(k>=300){ 
+      digitalWrite(buzzer,HIGH);
+      delay(200);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(200);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(200);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(200);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+      digitalWrite(buzzer,HIGH);
+      delay(1000);
+      digitalWrite(buzzer,LOW);
+      delay(100);
+     } 
+}
+}
+int abe(void){
+   // put your main code here, to run repeatedly:
+   //Initially trig is low 
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  //Then we make the trig HIGH
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(6);
+  digitalWrite(trig,LOW);
+
+  //Calculating the time duration for the signal
+  duration=pulseIn(echo,HIGH);
+  //Calculating the distance between the object and the person
+  distance=(duration*(0.032))/2;
+
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  return (distance);
+}
+
